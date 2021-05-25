d7cacc83fb2e65b17f8b39a70f777c0794e4a19e
Jose Carlos <josecarlos231998@gmail.com>
2021-05-25 17:03:17 -0600

    Sección de nosotros empezada.

--- js/index.js
+++ js/index.js
@@ -1,57 +1,41 @@
-var fondo = document.getElementById("img");
-let circulo1 = document.getElementById("circulo1");
-let circulo2 = document.getElementById("circulo2");
-let circulo3 = document.getElementById("circulo3");
-
-    function cambiarFondo(x) {
-
-        if (x == 1) {
-            fondo.src = "img/slide1.jpg";
-            circulo1.classList.add("circulito-relleno");
-            circulo2.classList.remove("circulito-relleno");
-            circulo3.classList.remove("circulito-relleno");
-        }else if(x == 2 ){
-            fondo.src = "img/slide2.jpg";
-            circulo2.classList.add("circulito-relleno");
-            circulo1.classList.remove("circulito-relleno");
-            circulo1.classList.remove("seleccionado");
-            circulo3.classList.remove("circulito-relleno");
-        }else if (x == 3){
-            fondo.src = "img/slide3.jpg";
-            circulo3.classList.add("circulito-relleno");
-            circulo1.classList.remove("circulito-relleno");
-            circulo2.classList.remove("circulito-relleno");
-        }
-    }
 
 
     // MENU FIJO
-    let menuchico = document.getElementById('menufijo');
-    let menu = document.getElementById('menu');
-    let logofijo = document.getElementById("img-fijo2");
-    let aaa = document.getElementById('imgg');
-    let logo = document.getElementById('logofijo');
-    let menu11= document.getElementById('menu11');
-    let fijo= document.getElementById('fijoo');
+    // let menuchico = document.getElementById('menufijo');
+    let menu = document.getElementById('menu-fijo');
+    let nav = document.getElementById('nav');
+    let menu1 = document.getElementById('navbarTogglerDemo03');
+    let carritoCompra = document.getElementById('carrito');
+   
 
     window.addEventListener('scroll', function(){
-    if((window.pageYOffset) > 35){
-        menu.classList.add('nav3');                                    
-        menu11.classList.add('cien');                                    
-        aaa.classList.add('mg');                                    
-        logo.classList.add('logo-fijo');      
-        logo.style.display = "block";                              
-        logofijo.style.display = "none";                              
-        fijo.classList.add('fijo');                                    
-    }
-    else{
-        menu11.classList.remove('cien');                                    
-        fijo.classList.remove('fijo');                                    
-        menu11.classList.remove('nav-opcionesM');                              
-        logofijo.style.display = "block";                              
-        menu.classList.remove('nav3');                                    
-        aaa.classList.remove('mg');                                    
-        logo.style.display = "none";                              
-    }
-
+        if((window.pageYOffset) > 35){
+            menu.classList.add('menu-fijo');                                    
+            nav.style.height="100px"       
+            carrito.style.top="110%"                             
+            
+        }else{
+            menu.classList.remove('menu-fijo');                                    
+            carrito.style.top="135%"                             
+            nav.style.height=""                                    
+                                
+        }
     })   
+
+
+    // ABRIR CARRITO
+
+
+        let iconoCar = document.getElementById('car')
+        let carrito = document.getElementById('carrito')
+        let cerrar = document.getElementById('cerrar')
+
+        
+        iconoCar.addEventListener('click', function(){
+                carrito.classList.add('show-car')
+        });
+        
+        cerrar.addEventListener('click', function(){
+                carrito.classList.remove('show-car')
+        });
+        
