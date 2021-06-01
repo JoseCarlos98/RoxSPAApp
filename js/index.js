caf2edddf43de964b89eb43e5b5530027a06881f
Jose Carlos <josecarlos231998@gmail.com>
2021-06-01 12:25:35 -0600

    Quick Fix

--- js/index.js
+++ js/index.js
@@ -38,4 +38,162 @@
         cerrar.addEventListener('click', function(){
                 carrito.classList.remove('show-car')
         });
+
+
+
+        // IMAGENES DE MUESTTRA DE INICIO 
+        const productos = [
+            {
+                imagen: '../img/maquillaje3.jpg',
+                nombre:'SEPHORA',
+                descripcion: 'Desmaquillante 16oz',
+                precio: '299.00'
+            },
+            {
+                imagen: '../img/maquillaje3.jpg',
+                nombre:'SEPHORA',
+                descripcion: 'Desmaquillante 16oz',
+                precio: '299.00'
+            },
+            {
+                imagen: '../img/maquillaje3.jpg',
+                nombre:'SEPHORA',
+                descripcion: 'Desmaquillante 16oz',
+                precio: '299.00'
+            },
+            {
+                imagen: '../img/maquillaje3.jpg',
+                nombre:'SEPHORA',
+                descripcion: 'Desmaquillante 16oz',
+                precio: '299.00'
+            },
+            {
+                imagen: '../img/maquillaje3.jpg',
+                nombre:'SEPHORA',
+                descripcion: 'Desmaquillante 16oz',
+                precio: '299.00'
+            },
+            {
+                imagen: '../img/maquillaje3.jpg',
+                nombre:'SEPHORA',
+                descripcion: 'Desmaquillante 16oz',
+                precio: '299.00'
+            },
+            {
+                imagen: '../img/maquillaje3.jpg',
+                nombre:'SEPHORA',
+                descripcion: 'Desmaquillante 16oz',
+                precio: '299.00'
+            },
+            {
+                imagen: '../img/maquillaje3.jpg',
+                nombre:'SEPHORA',
+                descripcion: 'Desmaquillante 16oz',
+                precio: '299.00'
+            },
+        ]
+
+        // IMAGENES DE MUESTRA DE PRODCTOS 
+        const imagenesProductos = [
+            {
+                imagen: '../img/maquillaje3.jpg',
+                nombre:'SEPHORA',
+                descripcion: 'Desmaquillante 16oz',
+                precio: '299.00'
+            },
+            {
+                imagen: '../img/maquillaje3.jpg',
+                nombre:'SEPHORA',
+                descripcion: 'Desmaquillante 16oz',
+                precio: '299.00'
+            },
+            {
+                imagen: '../img/maquillaje3.jpg',
+                nombre:'SEPHORA',
+                descripcion: 'Desmaquillante 16oz',
+                precio: '299.00'
+            },
+            {
+                imagen: '../img/maquillaje3.jpg',
+                nombre:'SEPHORA',
+                descripcion: 'Desmaquillante 16oz',
+                precio: '299.00'
+            },
+            {
+                imagen: '../img/maquillaje3.jpg',
+                nombre:'SEPHORA',
+                descripcion: 'Desmaquillante 16oz',
+                precio: '299.00'
+            },
+            {
+                imagen: '../img/maquillaje3.jpg',
+                nombre:'SEPHORA',
+                descripcion: 'Desmaquillante 16oz',
+                precio: '299.00'
+            },
+            {
+                imagen: '../img/maquillaje3.jpg',
+                nombre:'SEPHORA',
+                descripcion: 'Desmaquillante 16oz',
+                precio: '299.00'
+            },
+            {
+                imagen: '../img/maquillaje3.jpg',
+                nombre:'SEPHORA',
+                descripcion: 'Desmaquillante 16oz',
+                precio: '299.00'
+            },
+            {
+                imagen: '../img/maquillaje3.jpg',
+                nombre:'SEPHORA',
+                descripcion: 'Desmaquillante 16oz',
+                precio: '299.00'
+            },
+        ]
+        let htmlCodeI = '';
+        let htmlCodeP = '';
+        let imgsIndex = document.getElementById('imagenesIndex');
+        let imgsProductos = document.getElementById('imagenesProdutos');
         
+
+        addEventListener('load', ()=>{
+            console.log('cargo pantalla');
+            for (producto of imagenesIndex) {
+                htmlCodeI += `
+                <div class="col-md-3 largo" >
+                <div class="add-car">
+                <p>AGREGAR AL CARRITO</p>
+                </div>
+                <img src="${producto.imagen}" class="img-p" alt="">
+                <div class="info-p">
+                <p>${producto.nombre}</p>
+                <H2>${producto.descripcion}</H2>
+                <h1>${producto.precio}</h1>
+                </div>
+                </div>
+                `;
+            }
+            imgsIndex.innerHTML = htmlCodeI;
+        })
+
+        addEventListener('load', ()=>{
+            for (producto of imagenesProductos) {
+                htmlCodeP += `
+                <div class="col-md-3 largo" >
+                <div class="add-car">
+                <p>AGREGAR AL CARRITO</p>
+                </div>
+                <img src="${producto.imagen}" class="img-p" alt="">
+                <div class="info-p">
+                            <p>${producto.nombre}</p>
+                            <H2>${producto.descripcion}</H2>
+                            <h1>${producto.precio}</h1>
+                        </div>
+                    </div>
+                `;
+            }
+            imgsProductos.innerHTML = htmlCodeP;
+        })
+
+
+
